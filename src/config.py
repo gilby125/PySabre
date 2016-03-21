f42299e0d4a32fe5df3ec4483d8a898a043ddcb6
Bryan <gilby1@gmail.com>
2016-03-21 13:11:21 -0500

    ignore

--- src/config.py
+++ src/config.py
@@ -1,7 +1,25 @@
-clientID = ''
-password = ''
 
-auth_url  = 'https://api.test.sabre.com/v1/auth/token'
+clientID = 'V1:wks3oplgz4d8lc63:DEVCENTER:EXT'
+
+#clientID = 'V1:0uxeklwp3r0jy5ff:DEVCENTER:EXT'
+
+password = 'lcN4PhH3'
+#password = 'emD8Bv6G'
+
+auth_url = 'https://api.test.sabre.com/v1/auth/token'
 sabre_url = 'https://api.test.sabre.com'
+# https://api.test.sabre.com/v2/shop/flights/fares?
 
 access_token = ''
+
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': '192.168.1.8',
+    'port': '5432',
+    'username': 'pi',
+    'password': 'Lokifish123',
+    'database': 'sabre'
+}
+
+

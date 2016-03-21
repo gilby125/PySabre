5e87733ab5ddf17356344dc80d1b8dbd432fa361
Bryan <gilby1@gmail.com>
2016-03-21 13:23:32 -0500

    ignore

--- src/Destinationfindertest.py
+++ src/Destinationfindertest.py
@@ -54,36 +54,18 @@ for i in Src_airports:
 		cal.origin(i)
 		cal.pricepermile(cpm)
 		cal.lengthofstay([5, 6, 7, 8, 9, 10, 11, 12])
-		# cal.pointofsalecountry("US")
-
-		#print(cal.call())
-		#print(type(cal.call()))
 		print(i)
 		print(cal.call())
 		var = cal.call()
 
-		#print (cal.call())
-		#conn_string = "host='localhost' dbname='sabre' user='postgres' password='Lokifish123'"
-		#conn = psycopg2.connect(conn_string)
-		#cursor = conn.cursor()
-		#cursor.execute("insert into sabre_results (data) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",json.dumps([var],))
-		#cursor.execute("INSERT INTO sabre_results(data) VALUES (  %s)", json.dumps(var,))
-		#cursor.execute("INSERT INTO sabre_results(data) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", json.dumps(res,))
-		#cursor.execute("INSERT INTO product(store_id, url, price, charecteristics, color, dimensions) VALUES (%s, %s, %s, %s, %s, %s)", (55,  'http://www.google.com', '$20', cal.call(), 'red', '8.5x11'))
-		#time.sleep(3)
-		#conn.commit()
-		#cursor.close()
-		#pdb = dataset.connect()
-		#table = pdb['json_results']
+
 		print type(var)
 		with open('data.txt', 'w') as outfile:
 			json.dump(cal.call(), outfile, sort_keys = True, indent = 4,
 		ensure_ascii=False)
-		#table.insert(var)
 
 		db = client.sabre_results
 		fare = ({"author": "Bryan Gilbertson",
-	    #"fare": var,
 	    "origin": var['OriginLocation'],
 	    "Destination": var['FareInfo'][0]['DestinationLocation'],
 	    "lowfare": var['FareInfo'][0]['LowestFare'],
@@ -91,11 +73,10 @@ for i in Src_airports:
 		"DepartureDateTime": var['FareInfo'][0]['DepartureDateTime'],
 		"ReturnDateTime": var['FareInfo'][0]['ReturnDateTime'],
 	    "date": datetime.now()})
-		#fares = db.fares
+
 		print var['FareInfo'][0]['LowestFare']
 		result = db.sabre_cpm.insert_one(fare).inserted_id
-		#db.sabre_cpm.insert({ "created_date": datetime.now() })
-		#time.sleep(3)
+
 	except urllib2.HTTPError, e:
 		if e.code == 401:
 				print 'Unauthorized'
@@ -108,4 +89,3 @@ for i in Src_airports:
 		else:
 				print 'unknown error: '
 				print "ERROR: ", e.read()
-#conn.close()
